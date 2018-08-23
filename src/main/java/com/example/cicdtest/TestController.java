53cd49b9618e19b6d9cc88847acd3a4ecf919c4d
ssingularity <1755405701@qq.com>
2018-08-24 01:25:59 +0800

    hello cicd

--- src/main/java/com/example/cicdtest/TestController.java
+++ src/main/java/com/example/cicdtest/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/Test")
     public String test(){
-        return "hello world, hello ci/cd modified modifieds";
+        return "hello world, hello drone!";
     }
 }
